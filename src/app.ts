c1b2fd9ba8d6017faea358a3d8f548e44f5c72a7
karim-ch <chaari.karim.plus@gmail.com>
2021-02-13 00:18:40 +0100

    adding swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import { applyRoutes } from '@utils/router';
 import controllers from '@controllers';
+import swaggerUi from 'swagger-ui-express';
+import * as docs from './swagger.json';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(docs));
 
 applyRoutes(controllers, app);
 
